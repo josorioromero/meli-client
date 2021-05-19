77035ec8fd5493be994c23dc3a4e9520f6826634
josorioromero@gmail.com <josorio@arrivelogistics.com>
2021-05-19 08:12:48 -0500

    WIP search bar layout

--- src/components/search-bar/index.js
+++ src/components/search-bar/index.js
@@ -1,5 +1,7 @@
 // @vendors
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 // @styles
 import './styles.scss';
@@ -7,6 +9,10 @@ import './styles.scss';
 const SearchBar = () => (
     <div className="search-bar">
         <div className="search-bar__logo" />
+        <div className="search-bar__input">
+            <input className="search-bar__input-field" type="text" placeholder="Nunca dejes de buscar" />
+            <button className="search-bar__btn" />
+        </div>
     </div>
 );
 
