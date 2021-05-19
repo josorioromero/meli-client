4137007959372f0b61f79c38f98b91101ecaf417
josorioromero@gmail.com <josorio@arrivelogistics.com>
2021-05-19 07:30:04 -0500

    added initial layout for search bar and meli logo

---
+++ src/components/search-bar/index.js
@@ -0,0 +1,13 @@
+// @vendors
+import React from 'react';
+
+// @styles
+import './styles.scss';
+
+const SearchBar = () => (
+    <div className="search-bar">
+        <div className="search-bar__logo" />
+    </div>
+);
+
+export default SearchBar;
